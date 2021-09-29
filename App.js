53832967cc4a0c4951313209145a449588d20ef5
zz413012000 <zz413012000@gmail.com>
2021-09-29 12:49:46 +0800

    [Creat]First App

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+ import React, { Component } from 'react';
+ import {
+   StyleSheet,
+   View,
+   Text,
+ } from 'react-native';
+ 
+const App=()=>{
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>
+        Hello
+      </Text>
+    </View>
+  );
+}
+
+ export default App;
